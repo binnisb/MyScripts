7eb74d4e2a83d63d6701ce1f2e0edbf43d4d3186
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-01-23 11:31:52 +0100

    python preferres distribute to setuptools

---
+++ python/install_distribute_preferred.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Distribute preferred to setuptools, 
+# http://guide.python-distribute.org/installation.html
+
+wget http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
